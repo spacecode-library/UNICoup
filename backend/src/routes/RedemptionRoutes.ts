6d028f543aeccc02de50bf11156c6e330442b967
Planet Code <codelibrary21@gmail.com>
2025-03-23 15:41:40 +0200

    Update RedemptionRoutes.ts

--- backend/src/routes/RedemptionRoutes.ts
+++ backend/src/routes/RedemptionRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import RedemptionController from '../controllers/RedemptionController.js';
 import { StudentMW } from '../middleware/StudentMW.js'; // Middleware for student authentication
+import { MerchantMW } from '../middleware/MerchantMW.js';
 
 const router = Router();
 
@@ -9,5 +10,7 @@ router.post('/redeem-discount', StudentMW, RedemptionController.redeemDiscount);
 
 // Get redemption history for a student
 router.get('/redemption-history/:studentId', StudentMW, RedemptionController.getRedemptionHistory);
+
+router.post('/calculateMerchantFees/:discountId', MerchantMW, RedemptionController.calculateMerchantFees);
 
 export default router;
