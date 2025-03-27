a52cf6e15040c5b1c02ac9ef39f94d7245422136
Planet Code <codelibrary21@gmail.com>
2025-03-27 13:40:26 +0200

    Update EventRoutes.ts

--- backend/src/routes/EventRoutes.ts
+++ backend/src/routes/EventRoutes.ts
@@ -7,9 +7,9 @@ const router = Router();
 // Public Routes
 router.post('/create', StudentMW, EventController.createEvent);
 router.post('/', StudentMW, EventController.getEventData);
-router.post('/register', StudentMW, EventController.registeredEvent);
-router.get('/', StudentMW, EventController.getRegisteredStudents); // GET /event?id=eventId
-router.delete('/:id', StudentMW, EventController.deleteEvent); // DELETE /event/:id
-router.put('/:id', StudentMW, EventController.eventEdit); // PUT /event/:id
+router.post('/register', StudentMW, EventController.registeredEvent); // Changed to /register to avoid confusion
+router.get('/', StudentMW, EventController.getRegisteredStudents); // Matches GET /event?id=<eventId>
+router.delete('/:id', StudentMW, EventController.deleteEvent); // Matches DELETE /event/:id
+router.put('/:id', StudentMW, EventController.eventEdit); // Matches PUT /event/:id
 
 export default router;
